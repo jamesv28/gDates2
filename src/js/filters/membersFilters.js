ff0906f33eb946f29a94a6d4e4fd7c36f8ba986a
James Volmert <james.volmert@gmail.com>
2016-04-24 22:18:02 -0600

    added filters for age and gender

--- src/js/filters/membersFilters.js
+++ src/js/filters/membersFilters.js
@@ -1,3 +1,21 @@
-/**
- * Created by jamesvolmert on 4/24/16.
- */
+(function() {
+    //filter for gender
+    angular.module('myApp')
+        .filter('gender', function () {
+            return function (input) {
+                var genders = ['Male', 'Female', 'Other', 'Declined to Answer'];
+                return genders[input];
+            };
+        });
+    //filter for age
+    angular.module('myApp')
+        .filter('age', function() {
+            return function(input) {
+                if (!input) { return; }
+                var today = new Date().toString().split(' ')[3];
+                input = input.split('-')[0];
+                return today - input;
+            };
+        });
+
+})();
