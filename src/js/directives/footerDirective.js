14c5c9c5531fc60a12adc7b742af423dc6c4b220
James Volmert <james.volmert@gmail.com>
2016-04-24 21:24:18 -0600

    added footer directive

---
+++ src/js/directives/footerDirective.js
@@ -0,0 +1,15 @@
+angular.module('footerDirective', [])
+    .directive('footerInfo', function() {
+        return {
+            restrict: 'E',
+            //scope: {
+            //    member: '='
+            //},
+            transclude: true,
+            templateUrl: 'views/directives/footer.html',
+            controller: function($scope) {
+                //console.log('controller',$scope.member);
+            }
+        };
+    });  //end of directive
+
